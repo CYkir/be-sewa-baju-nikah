1cfc7f65794041ff8e955b626c0033e3f6b3bc6f
CYkir <mvzakiir@gmail.com>
2026-05-17 23:47:57 +0700

    fix: update PenyewaSerializer to specify fields explicitly

--- sewa_baju_nikah/api/serializers/penyewa_serializers.py
+++ sewa_baju_nikah/api/serializers/penyewa_serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 
-from sewa_baju_nikah_app.models import  Penyewa
+from sewa_baju_nikah_app.models import (
+    Penyewa
+)
 
 class PenyewaSerializer(serializers.ModelSerializer):
 
     class Meta:
+
         model = Penyewa
-        fields = '__all__'
+        fields = [
+            'id',
+            'nama_penyewa',
+            'nik',
+            'alamat',
+            'no_hp',
+            'jenis_kelamin',
+            'tanggal_daftar',
+            'status_data',
+            'created_at',
+            'updated_at',
+        ]
