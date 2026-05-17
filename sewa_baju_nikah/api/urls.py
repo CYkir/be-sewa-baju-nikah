540a5a389aee5c361fdac36c9115e95b941b692c
CYkir <mvzakiir@gmail.com>
2026-05-18 00:09:35 +0700

    fix: remove unused import of include in urls.py

--- sewa_baju_nikah/api/urls.py
+++ sewa_baju_nikah/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf import settings
 from django.conf.urls.static import static
