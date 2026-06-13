b8209b99ce80ba7706f953831fdf536b22bd843d
CYkir <mvzakiir@gmail.com>
2026-06-13 11:30:12 +0700

    feat: update requirements and add media URL handling in urls.py

--- sewa_baju_nikah/sewa_baju_nikah/urls.py
+++ sewa_baju_nikah/sewa_baju_nikah/urls.py
@@ -20,8 +20,17 @@ from api import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('' , include('api.urls',namespace= 'api')),
     path('api/', include('api.urls', namespace='api')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
